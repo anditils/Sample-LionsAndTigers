bd93cd1f0629b6ab773268d0a9e5b3a06c1716a6
Adrian Tillich <adrian.tillich@gmail.com>
2014-10-03 11:40:50 +0200

    initial UI setup

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +28,7 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+    }
 }
 

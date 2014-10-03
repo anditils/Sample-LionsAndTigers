49a8ebc5cf75496ca31c722cb309d02101672185
Adrian Tillich <adrian.tillich@gmail.com>
2014-10-03 11:51:22 +0200

    add struct for Tigers

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Adrian Tillich on 03.10.14.
+//  Copyright (c) 2014 Adrian Tillich. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")  // TODO add a default image so this does not cause problems
+}

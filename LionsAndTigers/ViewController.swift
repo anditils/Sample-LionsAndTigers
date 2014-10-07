5d0cfc9dcf211cca7408725eb5a5416e73f351d1
Adrian Tillich <adrian.tillich@gmail.com>
2014-10-07 22:50:08 +0200

    show random tigers from array when pressing the next button

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -16,27 +16,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var breedLabel: UILabel!
     
+    var myTigers:[Tiger] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         var firstTiger = Tiger()
         firstTiger.name = "Tigger"
         firstTiger.age = 3
         firstTiger.breed = "Bengal"
         firstTiger.image = UIImage(named: "BengalTiger.jpg")
-        
-        
-        println("my tiger's name is : \(firstTiger.name)")
-        println("my tiger's age is : \(firstTiger.age)")
-        println("my tiger's breed is : \(firstTiger.breed)")
-        println("my tiger's image is : \(firstTiger.image)")
+        myTigers.append(firstTiger)
         
         myImageView.image = firstTiger.image
         nameLabel.text = firstTiger.name
         ageLabel.text = "\(firstTiger.age)"
         breedLabel.text = firstTiger.breed
+        
+        var secondTiger = Tiger()
+        secondTiger.name = "Tigress"
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.age = 2
+        secondTiger.image = UIImage(named:"IndochineseTiger.jpg")
+        
+        var thirdTiger = Tiger()
+        thirdTiger.name = "Jacob"
+        thirdTiger.breed = "Malayan Tiger"
+        thirdTiger.age = 4
+        thirdTiger.image = UIImage(named:"MalayanTiger.jpg")
+        
+        var fourthTiger = Tiger()
+        fourthTiger.name = "Spar"
+        fourthTiger.breed = "Siberian Tiger"
+        fourthTiger.age = 5
+        fourthTiger.image = UIImage(named:"SiberianTiger.jpg")
+        
+        myTigers += [secondTiger, thirdTiger, fourthTiger]
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,6 +61,13 @@ class ViewController: UIViewController {
 
 
     @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        let index = Int(arc4random_uniform(UInt32(myTigers.count)))
+        let tiger = myTigers[index]
+        
+        myImageView.image = tiger.image
+        nameLabel.text = tiger.name
+        ageLabel.text = "\(tiger.age)"
+        breedLabel.text = tiger.breed
     }
 }
 
